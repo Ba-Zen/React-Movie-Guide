66fdecbb6e6cd6dea985711c9946cafa0b2b278e
Ba-Zen <bazenatlas@gmail.com>
2019-11-07 16:27:52 -0500

    updated app to display images and title

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React from 'react';
-import './App.css';
 import axios from 'axios';
+import './App.css';
 
 class App extends React.Component {
   state = {
-    movies: [],
-    img: []
+    movies: []
   };
 
   componentDidMount() {
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=0d134c0b91dab25d088582c2cef0e72b&language=en-US&page=1`;
     const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
-    const imgUrl = `https://api.themoviedb.org/3/configuration?api_key=0d134c0b91dab25d088582c2cef0e72b`;
-    Promise.all([axios.get(proxyUrl + url), axios.get(imgUrl)]).then(
-      ([movies, img]) => {
-        console.log(movies);
-        //const movies = res.data.results;
-        this.setState({ movies: movies.data.results, img: img.data.results });
-        // console.log('movies', movRes);
-        // console.log('img', imgRes);
-      }
-    );
+    Promise.all([axios.get(proxyUrl + url)]).then(([movies]) => {
+      this.setState({ movies: movies.data.results });
+    });
   }
 
   render() {
     const movies = this.state.movies;
-    console.log('MOVIES', movies.results);
-
     return (
-      <div>
-        {movies.map(movie => (
-          <>
-            <div>{movie.title}</div>
-          </>
+      <div className='grid-container'>
+        {movies.map((movie, idx) => (
+          <div key={idx}>
+            <img
+              src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}
+              alt={movie.title}
+              className='images'
+            />
+            <div className='title'>{movie.title}</div>
+          </div>
         ))}
       </div>
     );
