223d269760fdaac2bd999592eeeca1cb5d3de64f
Ba-Zen <bazenatlas@gmail.com>
2019-11-06 20:35:44 -0500

    connected to movie database, pulling in images and titles from server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    movies: [],
+    img: []
+  };
+
+  componentDidMount() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=0d134c0b91dab25d088582c2cef0e72b&language=en-US&page=1`;
+    const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
+    const imgUrl = `https://api.themoviedb.org/3/configuration?api_key=0d134c0b91dab25d088582c2cef0e72b`;
+    Promise.all([axios.get(proxyUrl + url), axios.get(imgUrl)]).then(
+      ([movies, img]) => {
+        console.log(movies);
+        //const movies = res.data.results;
+        this.setState({ movies: movies.data.results, img: img.data.results });
+        // console.log('movies', movRes);
+        // console.log('img', imgRes);
+      }
+    );
+  }
+
+  render() {
+    const movies = this.state.movies;
+    console.log('MOVIES', movies.results);
+
+    return (
+      <div>
+        {movies.map(movie => (
+          <>
+            <div>{movie.title}</div>
+          </>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
