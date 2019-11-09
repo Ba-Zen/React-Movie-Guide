bf85d138e1e1fe84136535042334c90df5b7b9a4
Ba-Zen <bazenatlas@gmail.com>
2019-11-09 13:34:35 -0500

    logo added to header

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { ReactComponent as Logo } from '../../assets/film.svg';
+
 import './header.styles.scss';
 
 const Header = () => (
   <div className='header'>
+    <div className='logo-container'>
+      <Logo className='logo' />
+    </div>
     <div className='options'>
       <Link className='options' to='/movies'>
         MOVIES
