d3a7ec92ff355605691bb2a3e9c25c65be7ee937
Ba-Zen <bazenatlas@gmail.com>
2019-11-09 13:01:13 -0500

    show list added

---
+++ src/components/show-list/show-list.component.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import axios from 'axios';
+import './tv-list.styles.scss';
+
+import ShowCard from '../show-card/show-card.component';
+
+class ShowList extends React.Component {
+  state = {
+    shows: []
+  };
+
+  componentDidMount() {
+    const url = `https://api.themoviedb.org/3/tv/popular?api_key=0d134c0b91dab25d088582c2cef0e72b&language=en-US&page=1`;
+    const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
+    Promise.all([axios.get(proxyUrl + url)]).then(([shows]) => {
+      this.setState({ shows: shows.data.results });
+    });
+  }
+
+  render() {
+    const shows = this.state.shows;
+    console.log(shows);
+    return (
+      <div className='list'>
+        {shows.map((show, idx) => (
+          <ShowCard key={idx} className='show-card' show={show} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShowList;
